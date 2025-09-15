7848bc0d9a943fd920aeb7fdc66881a7ff3c3e14
Yahir Salazar Ortiz <yahir.salazarortiz0113@gmail.com>
2025-09-15 17:37:53 -0600

    Initial Commit

---
+++ PrimerParcial_YahirSalazarOrtiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PrimerParcial_YahirSalazarOrtiz
+//
+//  Created by WIN603 on 15/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PrimerParcial_YahirSalazarOrtiz/PrimerParcial_YahirSalazarOrtizApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimerParcial_YahirSalazarOrtizApp.swift
+//  PrimerParcial_YahirSalazarOrtiz
+//
+//  Created by WIN603 on 15/09/25.
+//
+
+import SwiftUI
+
+@main
+struct PrimerParcial_YahirSalazarOrtizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
